ea85cbbc480b71a2729a001a897bb34531cc4f4a
Kevin Torres <kevintu236@gmail.com>
2025-12-12 16:48:30 -0600

    Instalación de Flowbite y dos componentes creados
    - Se instaló Flowbite en el proyecto para utilizar sus componentes.
    - Se creó el componente del header (Navbar.tsx) y el carrusel de la página de inicio (CarruselHome.tsx).

---
+++ feted/Componentes/CarruselHome.tsx
@@ -0,0 +1,85 @@
+import Image from "next/image"
+
+import imagen1 from "../public/carrusel-imagen-1.jpg";
+import imagen2 from "../public/carrusel-imagen-2.jpg";
+import imagen3 from "../public/carrusel-imagen-3.jpg";
+
+export default function CarruselHome() {
+    return (
+
+        <div id="default-carousel" className="relative w-full" data-carousel="slide">
+            <div className="relative h-36 overflow-hidden rounded-base md:h-96">
+                {/* Item 1 */}
+                <div className="hidden duration-700 ease-in-out items-center justify-center" data-carousel-item>
+                    <Image src={imagen1} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+
+                    {/* Opacidad para la imagen */}
+                    <div className="absolute flex items-center justify-center w-full h-full text-center bg-black opacity-50"></div>
+
+                    {/* Texto de la imagen */}
+                    <div className="absolute flex items-end justify-start w-full h-full text-center p-10">
+                        <div className="flex flex-col gap-2 items-start justify-start">
+                            <h1 className="text-4xl font-semibold">Unidos por un mejor futuro para todos.</h1>
+                            <p>Lorem ipsum dolor sit amet. Consectetur parle apropós de.</p>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Item 2 */}
+                <div className="hidden duration-700 ease-in-out" data-carousel-item>
+                    <Image src={imagen2} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+
+                    {/* Opacidad para la imagen */}
+                    <div className="absolute flex items-center justify-center w-full h-full text-center bg-black opacity-50"></div>
+
+                    {/* Texto de la imagen */}
+                    <div className="absolute flex items-end justify-start w-full h-full text-center p-10">
+                        <div className="flex flex-col gap-2 items-start justify-start">
+                            <h1 className="text-4xl font-semibold">Unidos por un mejor futuro para todos.</h1>
+                            <p>Lorem ipsum dolor sit amet. Consectetur parle apropós de.</p>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Item 3 */}
+                <div className="hidden duration-700 ease-in-out" data-carousel-item>
+                    <Image src={imagen3} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+
+                    {/* Opacidad para la imagen */}
+                    <div className="absolute flex items-center justify-center w-full h-full text-center bg-black opacity-50"></div>
+
+                    {/* Texto de la imagen */}
+                    <div className="absolute flex items-end justify-start w-full h-full text-center p-10">
+                        <div className="flex flex-col gap-2 items-start justify-start">
+                            <h1 className="text-4xl font-semibold">Unidos por un mejor futuro para todos.</h1>
+                            <p>Lorem ipsum dolor sit amet. Consectetur parle apropós de.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Puntitos indicadores*/}
+            <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+                <button type="button" className="w-3 h-3 rounded-base" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+                <button type="button" className="w-3 h-3 rounded-base" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+                <button type="button" className="w-3 h-3 rounded-base" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+            </div>
+
+            {/* Controles del carrusel */}
+            <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                <span className="inline-flex items-center justify-center w-10 h-10 rounded-base bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                    <svg className="w-5 h-5 text-white rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m15 19-7-7 7-7" /></svg>
+                    <span className="sr-only">Previous</span>
+                </span>
+            </button>
+            <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                <span className="inline-flex items-center justify-center w-10 h-10 rounded-base bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                    <svg className="w-5 h-5 text-white rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m9 5 7 7-7 7" /></svg>
+                    <span className="sr-only">Next</span>
+                </span>
+            </button>
+        </div>
+
+
+    )
+}
--- feted/app/layout.tsx
+++ feted/app/layout.tsx
@@ -24,10 +24,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link href="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.css" rel="stylesheet" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <script src="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.js"></script>
       </body>
     </html>
   );
